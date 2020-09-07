35b15dd374a941b14737ab4872a9b62d97334a28
Vichernand <64918184+Vichernand@users.noreply.github.com>
2020-09-06 22:24:59 -0300

    Add files via upload

--- Plot1.R
+++ Plot1.R
@@ -1,9 +1,5 @@
-library(ggplot2)
-
 data<-read.table("household_power_consumption.txt",sep = ";", header = TRUE)
 data <- subset(data,data$Date=="1/2/2007" |data$Date =="2/2/2007")
 
 hist(as.numeric(data$Global_active_power),col="red",main="Global Active Power",
      xlab="Global Active Power(kilowatts)")
-
-
--- Plot2.R
+++ Plot2.R
@@ -1,12 +1,10 @@
-library(ggplot2)
-
 data<-read.table("household_power_consumption.txt",sep = ";", header = TRUE)
 data <- subset(data,data$Date=="1/2/2007" |data$Date =="2/2/2007")
 
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 data$Time <- strptime(data$Time, format="%H:%M:%S")
-data[1:1440,"Time"] <- format(data[1:1440,"Time"],"2007-02-01 %H:%M:%S")
-data[1441:2880,"Time"] <- format(data[1441:2880,"Time"],
-                                     "2007-02-02 %H:%M:%S")
-plot(data$Time,as.numeric(as.character(data$Global_active_power)),
-     type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+data[1:1440,2] <- format(data[1:1440,2],"2007-02-01 %H:%M:%S")
+data[1441:2880,2] <- format(data[1441:2880,2],"2007-02-02 %H:%M:%S")
+
+plot(data$Time,as.numeric(data$Global_active_power),type="l",xlab="",
+     ylab="Global Active Power (kilowatts)") 
